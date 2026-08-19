f0b144267dd505d5220adf5eca2ac28aa99d4643
Priyanshu <priyanshu4096@gmail.com>
2026-08-19 12:04:19 +0530

    Fix Redis production connection

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -6,10 +6,10 @@ dotenv.config();
 export const RedisClient = createClient({
     username: process.env.REDIS_USERNAME || "default",
     password: process.env.REDIS_PASSWORD,
+
     socket: {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
-        tls: true,
     },
 });
 
